5e3c74148f09375bc8951ebb5e6134d56fee6f5c
Rain <78558736+ninetyeights@users.noreply.github.com>
2024-05-17 00:47:47 -0400

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "RainBlog",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "RainCoding",
+    links: {
+        twitter: "https://twitter.com",
+        github: "https://github.com",
+        personalSite: "https://personalsite.com",
+    }
+}
+
+export type SiteConfig = typeof siteConfig
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+export default {
     // othor next config here...
     webpack: config => {
         config.plugins.push(new VeliteWebpackPlugin())
