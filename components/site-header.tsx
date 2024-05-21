300dfb020bda1d986dd3c0fd61d2bbb371bc24a2
Rain <clover1998love@gmail.com>
2024-05-21 03:59:36 -0400

    Stage 4: Mobile Nav

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
-import {siteConfig} from '@/config/site';
-import {cn} from '@/lib/utils';
-import {buttonVariants} from '@/components/ui/button';
-import {Icons} from '@/components/icons'
-import {MainNav} from '@/components/main-nav';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import { Icons } from '@/components/icons'
+import { MainNav } from '@/components/main-nav';
+import { MobileNav } from './mobile-nav';
 
 export function SiteHeader() {
     return (
         <header
             className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-14 max-w-screen-2xl items-center">
-                <MainNav/>
+                <MainNav />
                 <div className="flex flex-1 items-center justify-end space-x-2">
                     <nav className="flex items-center">
                         <Link
@@ -20,11 +21,11 @@ export function SiteHeader() {
                         >
                             <div
                                 className={cn(
-                                    buttonVariants({variant: 'ghost'}),
+                                    buttonVariants({ variant: 'ghost' }),
                                     'w-10 px-0 hidden sm:inline-flex'
                                 )}
                             >
-                                <Icons.gitHub className="h-4 w-4"/>
+                                <Icons.gitHub className="h-4 w-4" />
                                 <span className="sr-only">GitHub</span>
                             </div>
                         </Link>
@@ -35,16 +36,16 @@ export function SiteHeader() {
                         >
                             <div
                                 className={cn(
-                                    buttonVariants({variant: 'ghost'}),
+                                    buttonVariants({ variant: 'ghost' }),
                                     'w-10 px-0 hidden sm:inline-flex'
                                 )}
                             >
-                                <Icons.twitter className="h-4 w-4"/>
+                                <Icons.twitter className="h-4 w-4" />
                                 <span className="sr-only">Twitter</span>
                             </div>
                         </Link>
                         {/*<ModeToggle/>*/}
-                        {/*<MobileNav/>*/}
+                        <MobileNav />
                     </nav>
                 </div>
             </div>
