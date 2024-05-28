589f63fa8fb382c73ded95b60481bae7ae3bc279
Rain <clover1998love@gmail.com>
2024-05-28 14:58:21 -0400

    Stage 10: Pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,39 +1,55 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-item";
+import { QueryPagination } from "@/components/query-pagination";
 import { sortPost } from "@/lib/utils";
 
-export default async function BlogPage() {
-  const sortedPosts = sortPost(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
+const POSTS_PER_PAGE = 3;
 
-  return (
-    <div className="container max-w-4xl  py-6 lg:py-10">
-      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
-        <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
-        <p className="text-xl text-muted-foreground">
-          My ramblings on all things web dev.
-        </p>
-      </div>
-      <hr className="mt-8" />
-      {displayPosts?.length > 0 ? (
-        <ul className="flex flex-col">
-          {displayPosts.map((post) => {
-            const { slug, date, title, description } = post;
-            return (
-              <li key={slug}>
-                <PostItem
-                  slug={slug}
-                  date={date}
-                  title={title}
-                  description={description}
-                />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p>Nothing to see here yet</p>
-      )}
-    </div>
-  );
+interface BlogPageProps {
+	searchParams: {
+		page?: string;
+	};
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+	const currentPage = Number(searchParams?.page) || 1;
+	const sortedPosts = sortPost(posts.filter((post) => post.published));
+	const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+	const displayPosts = sortedPosts.slice(
+		POSTS_PER_PAGE * (currentPage - 1),
+		POSTS_PER_PAGE * currentPage,
+	);
+
+	return (
+		<div className="container max-w-4xl  py-6 lg:py-10">
+			<div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+				<h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+				<p className="text-xl text-muted-foreground">
+					My ramblings on all things web dev.
+				</p>
+			</div>
+			<hr className="mt-8" />
+			{displayPosts?.length > 0 ? (
+				<ul className="flex flex-col">
+					{displayPosts.map((post) => {
+						const { slug, date, title, description } = post;
+						return (
+							<li key={slug}>
+								<PostItem
+									slug={slug}
+									date={date}
+									title={title}
+									description={description}
+								/>
+							</li>
+						);
+					})}
+				</ul>
+			) : (
+				<p>Nothing to see here yet</p>
+			)}
+			<QueryPagination totalPages={totalPages} className="justify-end mt-4" />
+		</div>
+	);
 }
